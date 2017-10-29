d3d2a8ae1defb0cebc193a2c047512f736673167
Emil Pettersson <emil.pettersson05@gmail.com>
2017-10-29 19:17:37 +0200

    Add from for messages and show incoming messages in browser

--- server/server.js
+++ server/server.js
@@ -19,9 +19,10 @@ io.on('connection', (socket) => {
 
     socket.broadcast.emit('newMessage', generateMessage('Admin', 'New user jouned'));
 
-    socket.on('createMessage', (message) => {
+    socket.on('createMessage', (message, callback) => {
         console.log('createMessage', message);
         io.emit('newMessage', generateMessage(message.from, message.text));
+        callback('This from the server');
         // io.emit('newMessage', {
         //     from: message.from,
         //     text: message.text,
